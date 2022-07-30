96be7b704610a8dfa8c7bd04a40a020afe40a606
BJuice <beetle_juice84@mail.ru>
2022-07-31 00:17:51 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const routes = require('./routes/index');
+const { ErrCodeServer } = require('./costants/constants');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT } = process.env;
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+const allowedCors = [
+  'http://api.mesto.bjuice.nomoredomains.xyz',
+  'http://mesto.bjuice.nomoredomains.xyz',
+  'https://api.mesto.bjuice.nomoredomains.xyz',
+  'https://mesto.bjuice.nomoredomains.xyz',
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(cors({
+  origin: allowedCors,
+  credentials: true,
+}));
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(requestLogger);
+
+app.use(routes);
+
+app.use(errorLogger);
+
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = ErrCodeServer, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === ErrCodeServer
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
