1a314e49cc0683e1ad6ceedb34090229a09089e5
BJuice <beetle_juice84@mail.ru>
2022-08-03 00:02:33 +0300

    fix/Добавить port по умолчанию

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const routes = require('./routes/index');
 const { ErrCodeServer } = require('./costants/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { NODE_ENV, PORT, MONGO_ADRESS } = process.env;
+const { NODE_ENV, PORT = 3000, MONGO_ADRESS } = process.env;
 
 const app = express();
 mongoose.connect(NODE_ENV === 'production' ? MONGO_ADRESS : 'mongodb://localhost:27017/moviesdb', {
